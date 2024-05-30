44e93c889dfd07b3475ab7cc3f61f577b63c1f31
Mudasir <ahmedmudassir392@gmail.com>
2024-05-30 18:50:34 +0500

    Add project files.

---
+++ LoginApplication/Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using LoginApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginApplication.Controllers
+
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserProfile objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (LoginApplicationEntities1 db = new LoginApplicationEntities1())
+                {
+                    var obj = db.UserProfiles.Where(a => a.Username.Equals(objUser.Username) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.UserId.ToString();
+                        Session["UserName"] = obj.Username.ToString();
+                        return RedirectToAction("UserDashBoard");
+                    }
+                }
+            }
+            return View(objUser);
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserId"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
---
+++ LoginApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoginApplication.MvcApplication" Language="C#" %>
